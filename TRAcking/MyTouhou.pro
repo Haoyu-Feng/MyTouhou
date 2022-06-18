fce576282b6a6d0164b08fee117d2bd6b68664fd
Neighhhbor <105040135+Neighhhbor@users.noreply.github.com>
2022-06-18 19:36:03 +0800

    实现我方子弹和地方子弹的追踪打击，出现的bug是中间的那个敌机不见了。。

---
+++ TRAcking/MyTouhou.pro
@@ -0,0 +1,33 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2022-05-15T16:27:35
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = MyTouhou
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+    plane.cpp \
+    stage.cpp \
+    movevector.cpp \
+    bullet.cpp \
+    enemy.cpp \
+    fly.cpp
+
+HEADERS  += \
+    plane.h \
+    config.h \
+    stage.h \
+    movevector.h \
+    bullet.h \
+    enemy.h \
+    fly.h
+
+RESOURCES += \
+    res.qrc
---
+++ TRAcking/enemy.cpp
@@ -0,0 +1,38 @@
+﻿#include "enemy.h"
+
+enemy::enemy()
+{
+}
+
+yousei::yousei(){Health=0;}
+
+yousei::yousei(int x_,int y_){
+    pic.load(":/res/yousei.png");
+    Width=pic.width();
+    Height=pic.height();
+    x=x_;
+    y=y_;
+    xspeed=0;
+    yspeed=1;
+    Health=100;
+    shoot_temp=0;
+    CD=10;
+    nowCD=-5;
+    for(int i=0;i<MAX_SHOOT;++i){
+        e_b[i]=enemy_bullet(x,y);
+    }
+}
+
+void yousei::enemy::shoot(){
+    ++nowCD;
+    if(nowCD<CD){
+        return;
+    }
+    nowCD=0;
+    e_b[shoot_temp].x=x;
+    e_b[shoot_temp].y=y;
+    if(shoot_temp<MAX_SHOOT-1){
+        e_b[shoot_temp++].Health=(1<<22);
+    }
+}
+
